54a3a88ed1beb4e1d750feff3d9646bdedb020b3
gisnaefde <gisnafauziande198@gmail.com>
2021-07-29 22:18:19 +0700

    Add icons Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Footer from "../parts/Footer";
 import Header from "../parts/Header";
+import Fb from "../assets/icons/fb.png";
+import Ig from "../assets/icons/ig.png";
+import Telegram from "../assets/icons/telegram.png";
+import Twitter from "../assets/icons/twitter.png";
+import Wa from "../assets/icons/wa.png";
 
 const Contact = () => {
   return (
@@ -9,6 +14,11 @@ const Contact = () => {
         <Header></Header>
         <div>
           <h1 className="text-6xl text-center text-white mt-10">My Contact</h1>
+          <img src={Fb} width={50} alt="/" className="mt-2 mb-2"></img>
+          <img src={Ig} width={50} alt="/"  className="mt-2 mb-2"></img>
+          <img src={Telegram} width={50} alt="/"  className="mt-2 mb-2"></img>
+          <img src={Twitter} width={50} alt="/"  className="mt-2 mb-2"></img>
+          <img src={Wa} width={50} alt="/"  className="mt-2 mb-2"s></img>
         </div>
         <Footer></Footer>
       </div>
