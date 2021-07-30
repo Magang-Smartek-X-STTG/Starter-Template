655e12450e1d5cac400584c063ef7cd0362702d4
WibyFabian08 <wibyfabian08@gmail.com>
2021-07-30 06:47:05 -0700

    pemanggilan global state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Footer from "../parts/Footer";
 import Header from "../parts/Header";
 
 const Home = () => {
+  const dataGlobal = useSelector((state) => state);
   return (
     <div className="h-screen">
       <div className="container mx-auto flex flex-col justify-between h-screen">
         <Header></Header>
         <div>
           <h1 className="text-6xl text-center text-white mt-10">Home Page</h1>
+          <p className="text-white text-center text-xl mt-5">Heloo my name is {dataGlobal.name}</p>
         </div>
         <Footer></Footer>
       </div>
