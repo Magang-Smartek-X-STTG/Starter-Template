e60fb8594fe054f238d1863de81f1fbddcba0fe7
WibyFabian08 <wibyfabian08@gmail.com>
2021-07-29 17:46:20 -0700

    Haloo Dunia

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ import './assets/styles/css/style.css';
 function App() {
   return (
     <div className="text-center bg-gray-500">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-6xl text-white">
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-      </header>
+      <h1>Haloo Dunia</h1>
     </div>
   );
 }
