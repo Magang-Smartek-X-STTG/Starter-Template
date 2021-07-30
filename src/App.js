758205eac6257be6492bf4b2488a921255e136aa
WibyFabian08 <wibyfabian08@gmail.com>
2021-07-29 17:33:14 -0700

    add postcss config

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './assets/styles/css/style.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-center bg-gray-500">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-6xl text-white">
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
       </header>
     </div>
